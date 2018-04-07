b2e873fe6c56d43f248ccb7f7057753c94fc0bdc
khory kotowski <kakkman525@gmail.com>
2018-04-06 21:11:49 -0400

    Added search functionality for the list of airports

--- src/pages/airports/airports.ts
+++ src/pages/airports/airports.ts
@@ -27,6 +27,8 @@ export class AirportsPage {
   public airportsInRange = [];
   public airports = [];
 
+  public queriedAirports = [];
+
   constructor(public navCtrl: NavController, 
   	public navParams: NavParams,
   	public dataProvider: DataProvider,
@@ -36,6 +38,7 @@ export class AirportsPage {
       enableHighAccuracy : false
     };
     this.airports = this.dataProvider.getAllAirports();
+    this.queriedAirports = this.airports;
     //this.loadAirportsInRange();
 
   }
@@ -52,6 +55,28 @@ export class AirportsPage {
     });
     //this.airportsInRange = this.dataProvider.getAirportsInRange(this.userlat, this.userlong)
   }
+
+//search function for finding a particular airports feed
+  searchAirports(searchbar) { 
+		// Reset items back to all of the items
+		this.queriedAirports = this.airports;
+
+		var query = searchbar.srcElement.value;// set q to the value of the searchbar
+		if (!query) { // if the value is an empty string don't filter the items
+		  return;
+			}
+		this.queriedAirports = this.queriedAirports.filter((result) => {
+			if(result.name && query) {
+		    	if (result.name.toLowerCase().indexOf(query.toLowerCase()) > -1) { //checks the string against the value of the name property
+		        	return true;
+		        }
+		        if (result.iata_code.toLowerCase().indexOf(query.toLowerCase()) > -1){
+		        	return true;
+		      	}
+		      	return false;
+		    }
+			});
+		}
 
 
   ionViewDidLoad() {
