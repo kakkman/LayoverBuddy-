00eae02c645b78b13e8f2e6b0d25e47434dc0203
khory kotowski <kakkman525@gmail.com>
2018-04-08 14:44:08 -0400

    added code for displaying active users at airport

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { DataProvider } from '../../providers/data/data';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,12 +12,16 @@ export class HomePage {
 	public airport;
 	public iata_code;
 	public name;
+
+	public activeUsers;
   constructor(public navCtrl: NavController,
-   public navParams: NavParams) {
+   public navParams: NavParams,
+   public dataProvider: DataProvider) {
 
   	this.airport = this.navParams.get('airport');
   	this.iata_code = this.airport.iata_code;
   	this.name = this.airport.name;
+  	this.activeUsers = this.dataProvider.getActiveUsersAtAirport(this.airport);
   }
 
 }
